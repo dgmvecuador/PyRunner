45fd61d9607f2b29653a63c025f53600762fe1a8
Russ <russplaysguitar@yahoo.com>
2011-10-30 23:47:25 -0700

    Removed hidden files

--- classes/.scroller.py
+++
@@ -1,8 +0,0 @@
-#super class for all scrolling objects
-class scroller(pygame.sprite.Sprite):
-    dx = 6
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-    def update(self):
-        self.rect.centerx -= self.dx
-    screen = None
